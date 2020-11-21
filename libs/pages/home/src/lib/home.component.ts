cf73ecf6c24734a6c8cfbd57e720a40ccaef9951
Aly Nasser <ng.alynasser@gmail.com>
2020-11-21 12:24:24 +0200

    feat: homepage

--- libs/pages/home/src/lib/home.component.ts
+++ libs/pages/home/src/lib/home.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  @HostBinding('class.homepage') homepageClass = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
